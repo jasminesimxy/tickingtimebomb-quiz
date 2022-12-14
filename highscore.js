043218b6b5cf47cb07c7e69274294695ee60d01e
jasmine <jasminesimxy@gmail.com>
2022-12-15 10:37:11 +1100

    added user initial variable onto my highscore.js file

--- highscore.js
+++ highscore.js
@@ -1,3 +1,15 @@
+var userInitials= document.getElementById("user-initials");
+
+function renderedLastRegistered (){
+    var initials =localStorage.getItem("initials");
+
+    if (!initials) {
+        return;
+    }
+
+    userInitials.textContent= initials;
+}
+
 
 //another page - highscore page ith local storage of  render last registered
 function highscoreList(){ 
