d62f33369d307bb2d5a6109d5b35f24a4452f3df
jasmine <jasminesimxy@gmail.com>
2022-12-18 16:37:20 +1100

    fixed all errors

--- highscores.js
+++ highscores.js
@@ -1,15 +1,21 @@
 var scoreHistory = JSON.parse(localStorage.getItem("scoreHistory")) || [];
 var clearHighscore = document.getElementById("clearhighscore")
+var headingsContainer = document.querySelector("tr");
+
+headingsContainer.setAttribute('style', 'font-size: 20px; color: black;font-weight: bolder; padding:100px;alignItems:center')
 
 for (let index = 0; index < scoreHistory.length; index++) {
     const element = scoreHistory[index];
 
-    //
+    //initials
     var initials = document.createElement('td')
     initials.textContent = scoreHistory[index].initials;
+    initials.setAttribute('style' , 'font-size: 20px; color:black;font-weight: bolder; padding:10px 10px;  alignItems:center') 
 
+    //scores
     var score = document.createElement('td')
     score.textContent = scoreHistory[index].score;
+    score.setAttribute('style' , 'font-size: 20px; color:black;font-weight:bolder;padding: 10px 10px;alignItems:center') 
 
     var tr = document.createElement('tr')
     tr.appendChild(initials)
@@ -26,3 +32,5 @@ clearHighscore.addEventListener('click',function() {
     localStorage.clear()
     document.getElementById("tbody").innerHTML= ""
 } );
+
+clearHighscore.setAttribute('style' , 'font-size: 20px; color:#fc1750;font-weight: bolder; padding-left: 30px; padding-right:30px;alignItems:center') 
